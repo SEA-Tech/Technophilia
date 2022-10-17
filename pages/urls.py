2bafbbdb390f4ae1d9c19bac36b12a6653737803
SEA-Tech <59169706+SEA-Tech@users.noreply.github.com>
2022-10-17 11:58:24 +0530

    Update urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, hospitals, register_hospital, hospital_page, donors_list, register_donation, use_blood_hospital, loginPage, logoutUser, search_for_blood, search_for_blood_result
+from .views import home, hospitals, register_hospital, hospital_page, donors_list, register_donation, use_blood_hospital, loginPage, logoutUser, search_for_blood, search_for_blood_result, register
 
 urlpatterns = [
     path('', home, name='home'),
@@ -12,5 +12,6 @@ urlpatterns = [
     path('login/', loginPage, name='login'),
     path('logout/', logoutUser, name='logout'),
     path('search/', search_for_blood, name='search'),
-    path('search/<str:blood_group>', search_for_blood_result, name='search-res')
+    path('search/<str:blood_group>', search_for_blood_result, name='search-res'),
+    path('register/', register, name='register'),
 ]
